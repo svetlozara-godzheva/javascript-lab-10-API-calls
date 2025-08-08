1e49f7d14c820ac94eb17f473a917e3ccdd701cb
svetlozara-godzheva <n01754024@humber.ca>
2025-08-08 18:25:44 -0400

    Completed task 2

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let fetchButton = document.getElementById("fetch-btn");
 let xhrButton = document.getElementById("xhr-btn");
+let title = document.getElementById("title");
+let body = document.getElementById("body");
+
 fetchButton.addEventListener("click", () => {
     fetch("https://jsonplaceholder.typicode.com/posts/1").then(response => {
         if (response.status != 200) {
@@ -8,15 +11,29 @@ fetchButton.addEventListener("click", () => {
         return response.json();
     }).then(data => {
         console.log(data);
-        displayResponse(data);
+        title.innerHTML = data.title;
+        body.innerHTML = data.body;
     }).catch(error => console.log("Error fetching post information", error));
 });
 
-function displayResponse(e) {
-    let title = document.getElementById("title");
-    title.innerHTML = e.title;
-    let body = document.getElementById("body");
-    body.innerHTML = e.body;
+xhrButton.addEventListener("click", () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/2", true);
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            console.log("Response:", xhr.responseText);
+            let data = JSON.parse(xhr.responseText);
+            title.innerHTML = data.title;
+            body.innerHTML = data.body;
+        } else {
+            alert("Server response was not 200");
+        }
+    };
 
+    xhr.onerror = () => {
+        alert("Error fetching post information");
+    };
 
-}
+    xhr.send();
+});
+
